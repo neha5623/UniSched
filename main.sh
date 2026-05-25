9172d49e0a8850aab7fd2e8d758e51fbf5689ba5
neha5623 <neha.naseer610@gmail.com>
2026-05-25 11:52:53 +0530

    timetable addition added

--- main.sh
+++ main.sh
@@ -12,7 +12,9 @@ do
 	echo "6. View Rooms"
 	echo "7. Add Timeslot"
 	echo "8. View Timeslots"
-    	echo "9. Exit"
+    	echo "9. Create Timetable"
+	echo "10. View Timetable"
+	echo "11. Exit"
     	echo "================================="
 	read -p "Enter choice: " choice
 	case $choice in 
@@ -60,6 +62,46 @@ do
 			cat data/slots.txt
 			;;
 		9)
+    			read -p "Enter Timetable ID: " TTid
+    			read -p "Enter Subject ID: " sid
+    			read -p "Enter Faculty ID: " fid
+    			read -p "Enter Room ID: " rid
+    			read -p "Enter Slot ID: " tid
+
+    			facultyClash=$(awk -F "|" -v f="$fid" -v s="$tid" '
+    			$3==f && $5==s {print $0}
+    			' data/timetable.txt)
+
+    			roomClash=$(awk -F "|" -v r="$rid" -v s="$tid" '
+    			$4==r && $5==s {print $0}
+    			' data/timetable.txt)
+
+    			if [ ! -z "$facultyClash" ]
+    			then
+        			echo "Faculty Clash Detected!"
+    
+    			elif [ ! -z "$roomClash" ]
+    			then
+        			echo "Room Clash Detected!"
+
+   		 	else
+       				echo "$TTid|$sid|$fid|$rid|$tid" >> data/timetable.txt
+        			echo "Timetable Added Successfully"
+    			fi
+    			;;
+		10)
+
+   			echo "==============================================="
+    			echo "TIMETABLE"
+   			echo "==============================================="
+
+    			awk -F "|" '{
+    			printf "TT_ID: %-5s Subject: %-5s Faculty: %-5s Room: %-5s Slot: %-5s\n",$1,$2,$3,$4,$5
+    			}' data/timetable.txt
+
+    			;;
+			
+		11)
 			echo "Exiting..."
 			;;
 		*)
@@ -70,4 +112,4 @@ do
 	read -p "Press Enter to continue.."
 	clear 
 done
-====
+
